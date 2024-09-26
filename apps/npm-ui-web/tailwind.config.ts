a29d57bd3675b56ea10199d197f62866697b9207
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-09-26 06:57:01 +0200

    Npm test web

---
+++ apps/npm-ui-web/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss';
+
+import twConfigBase from '@janribka/components/tailwind.config';
+
+const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
+  content: [
+    './src/features/**/*.{js,ts,jsx,tsx}',
+    './src/shared/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@janribka/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  presets: [twConfigBase],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#ff0000',
+          DEFAULT: '#aa0000',
+          dark: '#550000',
+        },
+      },
+    },
+  },
+};
+
+export default config;
