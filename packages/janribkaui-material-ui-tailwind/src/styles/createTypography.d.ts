3f966a4809264f8a0508ebda6d368d247abb7114
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-10-29 18:25:40 +0100

    [janribkaui/material-ui-tailwind][Checkbox] Create component Typography

--- packages/janribkaui-material-ui-tailwind/src/styles/createTypography.d.ts
+++ packages/janribkaui-material-ui-tailwind/src/styles/createTypography.d.ts
@@ -3,20 +3,14 @@ import * as React from 'react';
 
 import { Palette } from './createColors';
 
-export type Variant =
-  | 'h1'
-  | 'h2'
-  | 'h3'
-  | 'h4'
-  | 'h5'
-  | 'h6'
-  | 'subtitle1'
-  | 'subtitle2'
-  | 'body1'
-  | 'body2'
-  | 'caption'
-  | 'button'
-  | 'overline';
+export type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+// | 'subtitle1'
+// | 'subtitle2'
+// | 'body1'
+// | 'body2'
+// | 'caption'
+// | 'button'
+// | 'overline';
 
 export interface FontStyle {
   fontFamily: React.CSSProperties['fontFamily'];
