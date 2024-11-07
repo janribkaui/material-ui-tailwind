c1f83a69b5fbcbac7a7e75ef9ab386f54c4df646
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-11-07 18:54:55 +0100

    [janribkaui/material-ui-tailwind][FormHelperText] Create component FormHelperText

--- packages/janribkaui-material-ui-tailwind/src/FormHelperText/FormHelperText.js
+++ packages/janribkaui-material-ui-tailwind/src/FormHelperText/FormHelperText.js
@@ -10,11 +10,37 @@ import { mergeStyles } from '../utils';
 const FormHelperTextRoot = styled.p``;
 
 const formHelperTextRootVariants = tv({
-  base: ['text-text-secondary'],
+  base: [
+    'text-text-secondary',
+    'font-normal', // V2: Replace with caption from createTypography
+    'text-[0.75rem]', // V2: Replace with caption from createTypography
+    'leading-[1.66rem]', // V2: Replace with caption from createTypography
+    `tracking-[${round(0.4 / 12)}em]`, // V2: Replace with caption from createTypography
+    'text-left',
+    'mt-[0.188rem]',
+    'mr-0',
+    'mb-0',
+    'ml-0',
+    'disabled:text-text-disabled',
+  ],
+  variants: {
+    error: {
+      true: ['text-error'],
+      false: [],
+    },
+    size: {
+      small: ['mt-[0.25rem]'],
+      medium: [],
+    },
+    contained: {
+      true: ['ml-[0.875rem]', 'mr-[0.875rem]'],
+      false: [],
+    },
+  },
 });
 
 const FormHelperText = React.forwardRef(function FormHelperText(inProps, ref) {
-  const props = useDefaultProps({ props: inProps, name: 'MuiFormHelperText' });
+  const props = useDefaultProps({ props: inProps, name: 'JrFormHelperText' });
   const {
     children,
     className,
@@ -39,8 +65,17 @@ const FormHelperText = React.forwardRef(function FormHelperText(inProps, ref) {
   return (
     <FormHelperTextRoot
       as={component}
-      className={mergeStyles(formHelperTextRootVariants({}), className)}
       disabled={fcs.disabled}
+      className={mergeStyles(
+        'JrFormHelperText-root',
+        formHelperTextRootVariants({
+          error: fcs.error,
+          size: fcs.size,
+          contained: fcs.variant === 'filled' || fcs.variant === 'outlined',
+        }),
+        className,
+        '',
+      )}
       ref={ref}
       {...other}
     >
