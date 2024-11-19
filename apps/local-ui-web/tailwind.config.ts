16d9fa00e006cd6e5edb6c7d9355d1a60f237877
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-11-19 23:27:02 +0100

    Fix create colors

--- apps/local-ui-web/tailwind.config.ts
+++ apps/local-ui-web/tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'tailwindcss';
+import { blue } from '@janribkaui/material-ui-tailwind/colors';
+import { getContrastText } from '@janribkaui/material-ui-tailwind/styles';
 import twConfigBase from '@janribkaui/material-ui-tailwind/tailwind.config';
 
-const config: Pick<Config, 'content' | 'presets'> = {
+const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
   content: [
     './src/features/**/*.{js,ts,jsx,tsx}',
     './src/shared/**/*.{js,ts,jsx,tsx}',
@@ -10,6 +12,19 @@ const config: Pick<Config, 'content' | 'presets'> = {
     './node_modules/@janribkaui/material-ui-tailwind/**/*.{js,ts,jsx,tsx}',
   ],
   presets: [twConfigBase],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: 'rgb(254, 205, 210)',
+          DEFAULT: 'rgb(253, 126, 143)',
+          dark: 'rgb(227, 113, 128)',
+          contrastText: getContrastText('rgb(238, 238, 238)'),
+        },
+        blue,
+      },
+    },
+  },
 };
 
 export default config;
