59297b1535b6e50130d70ce82da36631c261ce49
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-11-24 01:08:32 +0100

    [janribkaui/material-ui-tailwind] Create a helper function for merging base and Tailwind configuration (#75)
    * Fix create colors
    
    * Fix create colors
    
    * Update README.md
    
    * Update README.md
    
    * Delete changes from tailwind config

--- packages/janribkaui-material-ui-tailwind/src/styles/createColors.d.ts
+++ packages/janribkaui-material-ui-tailwind/src/styles/createColors.d.ts
@@ -148,7 +148,7 @@ export interface PaletteOptions {
   // getContrastText?: (background: string) => string;
 }
 
-export function getContrastText(background: string, contrastThreshold: number): string;
+export function getContrastText(background: string, contrastThreshold?: number): string;
 
 // TODO: V modelu palette chybí dark
 export default function createColors(): ResolvableTo<RecursiveKeyValuePair<keyof Palette, string>>;
