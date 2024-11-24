9de16f33d851532f5ffc308d40230df940f68033
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-11-24 01:05:07 +0100

    Delete changes from tailwind config

--- apps/local-ui-web/tailwind.config.ts
+++ apps/local-ui-web/tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from 'tailwindcss';
-import { blue } from '@janribkaui/material-ui-tailwind/colors';
-import { getContrastText } from '@janribkaui/material-ui-tailwind/styles';
 import twConfigBase from '@janribkaui/material-ui-tailwind/tailwind.config';
 
-const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
+const config: Pick<Config, 'content' | 'presets'> = {
   content: [
     './src/features/**/*.{js,ts,jsx,tsx}',
     './src/shared/**/*.{js,ts,jsx,tsx}',
@@ -12,19 +10,6 @@ const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
     './node_modules/@janribkaui/material-ui-tailwind/**/*.{js,ts,jsx,tsx}',
   ],
   presets: [twConfigBase],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          light: 'rgb(254, 205, 210)',
-          DEFAULT: 'rgb(253, 126, 143)',
-          dark: 'rgb(227, 113, 128)',
-          contrastText: getContrastText('rgb(253, 126, 143)'),
-        },
-        blue,
-      },
-    },
-  },
 };
 
 export default config;
