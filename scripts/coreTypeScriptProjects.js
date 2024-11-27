4def54cd326cba192c64a088630b43aff41b61c8
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-11-27 22:16:25 +0100

    Add proptypes generating script

---
+++ scripts/coreTypeScriptProjects.js
@@ -0,0 +1,20 @@
+import path from 'path';
+
+export default {
+  material: {
+    rootPath: path.join(process.cwd(), 'packages/janribkaui-material-ui-tailwind'),
+    entryPointPath: 'src/index.d.ts',
+  },
+  // lab: {
+  //   rootPath: path.join(process.cwd(), 'packages/janribkaui-lab'),
+  //   entryPointPath: 'src/index.d.ts',
+  // },
+  system: {
+    rootPath: path.join(process.cwd(), 'packages/janribkaui-system'),
+    entryPointPath: 'src/index.d.ts',
+  },
+  docs: {
+    rootPath: path.join(process.cwd(), 'docs'),
+    tsConfigPath: 'tsconfig.json',
+  },
+};
